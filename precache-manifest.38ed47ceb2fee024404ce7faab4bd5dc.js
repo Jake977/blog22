f114999fb02c4253d628deafe0fb164fb62a9cb1
Eugene <openmind1377@gmail.com>
2020-10-26 15:10:14 +0300

    Updates

---
+++ precache-manifest.38ed47ceb2fee024404ce7faab4bd5dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a598aec91196ef189c86f0e890c5ae",
+    "url": "/blog22/index.html"
+  },
+  {
+    "revision": "ea8644a42bbc8c5c4793",
+    "url": "/blog22/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "06df3e84b6515e2089ef",
+    "url": "/blog22/static/css/main.7ae487a7.chunk.css"
+  },
+  {
+    "revision": "ea8644a42bbc8c5c4793",
+    "url": "/blog22/static/js/2.09a08975.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/blog22/static/js/2.09a08975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06df3e84b6515e2089ef",
+    "url": "/blog22/static/js/main.912c7fed.chunk.js"
+  },
+  {
+    "revision": "8411bc16c37c8832c185",
+    "url": "/blog22/static/js/runtime-main.ac6cde4a.js"
+  }
+]);
--- src/services/storageService.js
+++
@@ -1,8 +0,0 @@
-const storageService = {
-    setToLocalStorage: (key, item) =>
-        window.localStorage.setItem(key, item),
-    getFromLocalStorage: (key) =>
-        window.localStorage.getItem(key),
-};
-
-export default storageService;
